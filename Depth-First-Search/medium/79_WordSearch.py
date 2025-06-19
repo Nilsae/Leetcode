a68f33639d1b127502280392a456777f71462801
Nilsae <nilousaeedi@gmail.com>
2025-06-18 22:25:29 -0700

    DFS word search ok

--- Depth-First-Search/medium/79_WordSearch.py
+++ Depth-First-Search/medium/79_WordSearch.py
@@ -6,36 +6,30 @@ class Solution(object):
         :rtype: bool
         """
         initials_list = []
-        for i in range(len(board)):
-            for j in range(len(board[0])):
-                if board[i][j] == word[0]:
-                    initials_list.append((i, j))
-        answers = []
-        for i, j in initials_list:
-            visiting = set()
-            word_up_to_now = [word[0]]
-            self.DFS(board, i, j, word, word_up_to_now, visiting, answers)
-        print(answers)
-        for answer in answers:
-            if answer:
-                return True
-        return False
-    def DFS(self, board, i, j, word, word_up_to_now, visiting, answers):
-        if len(word_up_to_now) == len(word):
-            answers.append(word_up_to_now[:])
-            return word_up_to_now
         i_max = len(board)
         j_max = len(board[0])
+        for i in range(i_max):
+            for j in range(j_max):
+                if board[i][j] == word[0]:
+                    initials_list.append((i, j))
+        # answers = []
+        for i, j in initials_list:
+            visiting = set()
+            current_index = 0
+            if self.DFS(board, i, j, word, current_index, visiting, i_max, j_max):
+                return True
+        return False
+    def DFS(self, board, i, j, word, current_index, visiting, i_max, j_max):
+        if current_index == len(word) - 1:
+            # answers.append(word_up_to_now[:])
+            return True
         if (i, j) in visiting:
-            return
+            return False
         visiting.add((i, j))
         neighbors = [(i+1 ,j), (i-1, j), (i, j+1), (i, j-1)]
-        for neighbor in neighbors:
-            if neighbor[0] >= 0 and neighbor[0] < i_max and neighbor[1] >= 0 and neighbor[1] < j_max and board[neighbor[0]][neighbor[1]] == word[len(word_up_to_now)] and (neighbor[0], neighbor[1]) not in visiting:
-                    next_char = word[len(word_up_to_now)]
-                    word_up_to_now.append(next_char)
-                    self.DFS(board, neighbor[0], neighbor[1], word, word_up_to_now, visiting, answers)
-                    word_up_to_now.pop()
+        for n_i, n_j in neighbors:
+            if n_i >= 0 and n_i < i_max and n_j >= 0 and n_j < j_max and board[n_i][n_j] == word[current_index+1] and (n_i, n_j) not in visiting:
+                    if self.DFS(board, n_i, n_j, word, current_index + 1, visiting, i_max, j_max):
+                        return True
         visiting.remove((i, j))
-        return
-Solution().exist(board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCB")
+        return False
