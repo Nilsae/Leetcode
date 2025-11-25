b0a21cdff1e01c01003ac272850fe3535c85ebd6
Nilsae <nilousaeedi@gmail.com>
2025-11-24 17:28:47 -0800

    sliding window

--- DynamicProgramming/medium/53_MaximumSubarray.py
+++ DynamicProgramming/medium/53_MaximumSubarray.py
@@ -4,26 +4,40 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        if nums == []:
-            return []
+        # if nums == []:
+        #     return []
+        # left = 0
+        # right = 0
+        # max_sum = nums[0]
+        # current_sum = nums[0]
+        # r = l = 0
+        # for right in range(1, len(nums)):
+        #     if current_sum < 0:
+        #         current_sum = nums[right]
+        #         left = right
+        #     else:
+        #         current_sum += nums[right]
+            
+        #     if current_sum > max_sum:
+        #         max_sum = current_sum
+        #         r = right
+        #         l = left
+            
+        # # print(nums[l:r+1])
+        # return max_sum
+        
+        #polished:
         left = 0
-        right = 0
-        max_sum = nums[0]
-        current_sum = nums[0]
-        r = l = 0
+        cur_sum = 0
+        max_sum = cur_sum = nums[0]
         for right in range(1, len(nums)):
-            if current_sum < 0:
-                current_sum = nums[right]
+            if cur_sum < 0 :
                 left = right
+                cur_sum = nums[right]
             else:
-                current_sum += nums[right]
-            
-            if current_sum > max_sum:
-                max_sum = current_sum
-                r = right
-                l = left
-            
-        # print(nums[l:r+1])
+                cur_sum = cur_sum + nums[right]
+            if max_sum < cur_sum:
+                max_sum = cur_sum
         return max_sum
         
         
