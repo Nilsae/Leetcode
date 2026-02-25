4a045510a098a2feb9c6c00cc460885e40cacda3
Niloo <nilousaeedi@gmail.com>
2026-02-24 16:03:07 -0800

    codesignal practices Math

---
+++ Math/codesignal_are_coprime.py
@@ -0,0 +1,16 @@
+
+
+
+def are_coprime(a, b):
+    n = a
+    if b > a:
+        n = b
+    # n = int(n ** 0.5)
+    for i in range(2, n):
+        while a % i == 0 and b % i == 0:
+            a //= i
+            b //= i
+            return False
+    return True
+
+# This is simple. The efficient implementation is greatest common divisor (GCD) algorithm
