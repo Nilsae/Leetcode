777e63619ddf94036202de621eb38261255e92bb
Niloo <nilousaeedi@gmail.com>
2026-03-23 15:18:17 -0700

    decision tree

---
+++ ML/decision_tree_gini.py
@@ -0,0 +1,38 @@
+def test_split(index, value, dataset):
+    left, right = list(), list()
+    for row in dataset:
+        if row[index] < value:
+            left.append(row)
+        else:
+            right.append(row)
+    return left, right
+
+def gini_index(groups, classes):
+    total_instances = sum([len(group) for group in groups])
+    gini = 0.0
+    for group in groups:
+        size = float(len(group))
+        if size == 0:
+            continue
+        score = sum([(group.count(class_val) / size) ** 2 for class_val in classes])
+        gini += (1 - score) * (size / total_instances)
+    return gini
+
+def get_split(dataset):
+    class_values = list(set(row[-1] for row in dataset))
+    b_index, b_value, b_score, b_groups = 999, 999, 999, None
+    for index in range(len(dataset[0])-1):
+        for row in dataset:
+            # call the test_split function to get groups
+            groups = test_split(index, row[index], dataset)
+            # calculate the gini index using the gini_index function
+            gini = gini_index(groups, class_values)
+            if gini < b_score:
+                # update best index, best value, best score and best groups
+                b_index, b_value, b_score, b_groups = index, row[index], gini, groups
+    return {'index': b_index, 'value': b_value, 'groups': b_groups}
+
+# Dataset: Age, Preferred Genre, Likelihood to Watch
+dataset = [[16, 'Comedy', 'Yes'], [21, 'Action', 'No'], [25, 'Comedy', 'Yes']]
+split = get_split(dataset)
+print('Best Split: Column Index:', split['index'], 'Value:', split['value'])
