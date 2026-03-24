110fc8546a4a94b92b6e22d69ae76b94c1755dc5
Niloo <nilousaeedi@gmail.com>
2026-03-24 14:03:51 -0700

    GD with momentum

---
+++ ML/GD-with-momentum.py
@@ -0,0 +1,38 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def func(x):   
+    return x**2
+
+def grad_func(x): 
+    return 2*x
+
+alpha = 0.9
+learning_rate = 0.01
+momentum = 0
+epochs = 50
+
+theta_plain = 4.0  
+theta_momentum = 4.0
+
+history_plain = []    
+history_momentum = []    
+
+for _ in range(epochs):
+    history_plain.append(theta_plain)
+    gradient = grad_func(theta_plain)
+    theta_plain = theta_plain - learning_rate * gradient
+
+    history_momentum.append(theta_momentum)
+    gradient = grad_func(theta_momentum)
+    momentum = alpha * momentum + learning_rate * gradient
+    theta_momentum = theta_momentum - momentum
+
+plt.figure(figsize=(12, 7))
+plt.plot([func(theta) for theta in history_plain], label='Gradient Descent')
+plt.plot([func(theta) for theta in history_momentum], label='Momentum-based Gradient Descent')
+plt.xlabel('Epoch')
+plt.ylabel('Cost')
+plt.legend()
+plt.grid()
+plt.show()
